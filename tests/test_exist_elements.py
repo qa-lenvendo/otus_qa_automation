65c092c3d92b8c7ab06fcd31e18f66ed61c7fe3a
a_smoliak <FnxwNI3h7vLctM9Cg6>
2022-02-22 18:57:13 +0300

    style fix

--- tests/test_exist_elements.py
+++ tests/test_exist_elements.py
@@ -8,33 +8,23 @@ class TestCheckExistMainPage:
 
     def test_check_search_input_on_main_page(self, browser, base_url):
         browser.get(base_url)
-        strategy, locator = Locators.SEARCH_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.SEARCH_INPUT), 'element "Search Input" is not present'
 
     def test_check_banner_section(self, browser, base_url):
         browser.get(base_url)
-        strategy, locator = Locators.BANNER_SECTION
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.BANNER_SECTION), 'element "Search Input" is not present'
 
     def test_check_main_menu(self, browser, base_url):
         browser.get(base_url)
-        strategy, locator = Locators.MAIN_MENU
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.MAIN_MENU), 'element "Search Input" is not present'
 
     def test_check_small_basket(self, browser, base_url):
         browser.get(base_url)
-        strategy, locator = Locators.SMALL_BASKET
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.SMALL_BASKET), 'element "Search Input" is not present'
 
     def test_check_logo(self, browser, base_url):
         browser.get(base_url)
-        strategy, locator = Locators.LOGO
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.LOGO), 'element "Search Input" is not present'
 
 
 class TestCheckExistProductPage:
@@ -45,33 +35,23 @@ class TestCheckExistProductPage:
 
     def test_check_add_cart_button(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.ADD_CART_BUTTON
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.ADD_CART_BUTTON), 'element "Search Input" is not present'
 
     def test_check_product_price(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.PRODUCT_PRICE
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.PRODUCT_PRICE), 'element "Search Input" is not present'
 
     def test_check_product_name(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.PRODUCT_NAME
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.PRODUCT_NAME), 'element "Search Input" is not present'
 
     def test_check_description_tab(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.DESCRIPTION_TAB
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.DESCRIPTION_TAB), 'element "Search Input" is not present'
 
     def test_check_quantity_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.QUANTITY_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.QUANTITY_INPUT), 'element "Search Input" is not present'
 
 
 class TestCheckExistCatalogPage:
@@ -82,33 +62,23 @@ class TestCheckExistCatalogPage:
 
     def test_check_product_card(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.PRODUCT_CARD
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.PRODUCT_CARD), 'element "Search Input" is not present'
 
     def test_check_catalog_menu(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.CATALOG_MENU
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.CATALOG_MENU), 'element "Search Input" is not present'
 
     def test_check_description_page(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.DESCRIPTION
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.DESCRIPTION), 'element "Search Input" is not present'
 
     def test_check_quantity_filter(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.QUANTITY_FILTER
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.QUANTITY_FILTER), 'element "Search Input" is not present'
 
     def test_check_sort_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.SORT_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.SORT_INPUT), 'element "Search Input" is not present'
 
 
 class TestCheckExistAdminLoginPage:
@@ -119,33 +89,23 @@ class TestCheckExistAdminLoginPage:
 
     def test_check_logo(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.HEADER_LOGO
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.HEADER_LOGO), 'element "Search Input" is not present'
 
     def test_check_user_name_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.USER_NAME_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.USER_NAME_INPUT), 'element "Search Input" is not present'
 
     def test_check_password_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.PASSWORD_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.PASSWORD_INPUT), 'element "Search Input" is not present'
 
     def test_check_login_button(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.LOGIN_BUTTON
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.LOGIN_BUTTON), 'element "Search Input" is not present'
 
     def test_check_footer(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.FOOTER
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.FOOTER), 'element "Search Input" is not present'
 
 
 class TestCheckExistRegistrationUserPage:
@@ -156,30 +116,20 @@ class TestCheckExistRegistrationUserPage:
 
     def test_check_firstname_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.FIRSTNAME_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.FIRSTNAME_INPUT), 'element "Search Input" is not present'
 
     def test_check_lastname_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.LASTNAME_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.LASTNAME_INPUT), 'element "Search Input" is not present'
 
     def test_check_email_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.EMAIL_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.EMAIL_INPUT), 'element "Search Input" is not present'
 
     def test_check_telephone_input(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.TELEPHONE_INPUT
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.TELEPHONE_INPUT), 'element "Search Input" is not present'
 
     def test_check_continue_button(self, browser, base_url):
         browser.get(base_url + self._path)
-        strategy, locator = Locators.CONTINUE_BUTTON
-
-        assert UiHelper.is_element_present(browser, strategy, locator), 'element "Search Input" is not present'
+        assert UiHelper.is_element_present(browser, *Locators.CONTINUE_BUTTON), 'element "Search Input" is not present'
